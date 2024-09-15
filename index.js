552ba3f959457cd053628a16f1033248ef288bf0
Haku766 <jguilherme.camargo766@gmail.com>
2024-09-15 14:44:03 -0300

    algumas mudanças e adição de um novo crud

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
 
 app.use(express.static('./pages'));
 
-const port = 5501;
+const usuario = [];
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
+const router = express.Router();
+router.get('/api/usuario', (req, res) => {  
+    console.log('entrou no get');
+    res.status(200).json(usuario);
+});
+router.post('/api/usuario', (req, res) => {  
+    console.log('entrou no post');
+    console.log(req.body);
+
+    var usuario = req.body;
+    usuario.id = 1;
+
+    usuario.push(usuario);
+    res.status(201).json(usuario);
 });
 
+app.use(router);
+
+const port = 5501;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
